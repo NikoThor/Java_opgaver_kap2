53941f8f9d0e73c9fda4bf08f2dfd5d4e1d07302
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-09-19 20:16:51 +0200

    lavet det simplere :)

--- src/Opgave_2_17/Opgave_2_17.java
+++ src/Opgave_2_17/Opgave_2_17.java
@@ -4,11 +4,19 @@ import java.util.Scanner;
 
 public class Opgave_2_17 {
     public static void main(String[] args) {
+
+
+
         System.out.println("Indtast temp");
         Scanner input = new Scanner(System.in);
-        int temp = input.nextInt();
+        firstTemperature.setTemp(input.nextInt());
 
 
 
+
+    }
+    public int tempCall(int temperature , int windSpeed) {
+
+        return 1;
     }
 }
