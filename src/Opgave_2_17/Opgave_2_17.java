71a0acc7baedf12a089e2ec6387241bea48105f4
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-09-19 21:02:09 +0200

    Ved at sætte begrænsninger

--- src/Opgave_2_17/Opgave_2_17.java
+++ src/Opgave_2_17/Opgave_2_17.java
@@ -7,16 +7,27 @@ public class Opgave_2_17 {
 
 
 
-        System.out.println("Indtast temp");
+        System.out.println("insert a temperature between -58 and 41 fahrenheit");
         Scanner input = new Scanner(System.in);
-        firstTemperature.setTemp(input.nextInt());
-
+        if (!(input.nextDouble() <= 41) && !(input.nextDouble() >= -58)) {
+            System.out.println("Error cannot be below -58 or above 41 fahrenheit." + "");
+        }
+        double temp = input.nextDouble();
+        System.out.println("insert wind speed above 2 miles per hour");
+        double windSpeed = input.nextDouble();
+        double NWS = tempCall(temp,windSpeed);
+        System.out.println("How cold is it outside? here is the answer in wind-chill temperature: " + NWS);
 
 
 
     }
-    public int tempCall(int temperature , int windSpeed) {
+    public static double tempCall(double temperature , double windSpeed) {
 
-        return 1;
+
+        double temp1 = 35.74+ 0.6215*temperature;
+        double temp2 = Math.pow(35.75*windSpeed,0.16)+Math.pow(0.4275*temperature*windSpeed,0.16);
+        double sum = temp1 - temp2;
+        //35.74 + 0.6215 * temperature - Math.pow(35.75*windSpeed,0.16) + Math.pow(0.4275 * temperature * windSpeed, 0.16);
+        return sum;
     }
 }
