c93c4cd264a5abee0d4c83202d5ceca20464dc17
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-09-19 16:41:26 +0200

    Lavet metode og scanners

--- src/opgave2_12/opgave2_12.java
+++ src/opgave2_12/opgave2_12.java
@@ -6,11 +6,11 @@ public class opgave2_12 {
     public static void main(String[] args) {
 
         Scanner input1 = new Scanner(System.in);
-        System.out.println("****** indtast flyets acceleration ******");
+        System.out.println("****** indtast flyets acceleration i  m/s ******");
         double acceleration = input1.nextDouble();
 
         Scanner input2 = new Scanner(System.in);
-        System.out.println("****** indtast flyets takeoff hastighed i  ******");
+        System.out.println("****** indtast flyets takeoff hastighed i m/s^2  ******");
         double takeoff = input2.nextDouble();
 
 
